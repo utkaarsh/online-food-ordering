e9ffd69355aec83c3ea628f3ba20f29ee9de28d1
utkaarsh <utkarshranpise29@gmail.com>
2023-12-20 23:11:48 +0530

    added default imgs for items if api has some issues

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -14,7 +14,7 @@ const ItemList=({item})=>{
                     </div>
 
                     <div className="w-3/12 p-2 ">
-                <img src={CDN_IMG_URL+p.card.info.imageId} className=" w-[150px]  border border-gray-300 rounded-lg " alt="food-img" />
+                <img src={p.card.info.imageId?CDN_IMG_URL+p.card.info.imageId:"https://png.pngtree.com/element_our/20200702/ourmid/pngtree-vector-illustration-knife-and-fork-western-food-plate-image_2283844.jpg"} className=" w-[150px]  border border-gray-300 rounded-lg " alt="food-img" />
                 <button className="p-2 shadow-lg bg-green-100 w-15 h-8 p-2 rounded-lg"> Add + </button>
                 </div>
                 </div>
